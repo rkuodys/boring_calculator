e7a1744fdc4108d8335ddc6da475fb0553f6dcee
Rimantas <r.kuodys@gmail.com>
2023-03-24 20:45:13 +0200

    fixing imports

--- src/boring_calculator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    Calculator package to perform simple operations of addition, subtraction,
-    multiplication and division In addition it supports n-th root of number.
-    All actions are performed agains in-memory saved number.
-    Default state is 0.
-"""
-__version__ = "0.1"
